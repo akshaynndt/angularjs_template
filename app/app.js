f5c051fc01dfda3c5bc6bbcb6710c6863d06ed8e
akshaynndt <akshaynndt@gmail.com>
2017-05-09 11:19:47 +0530

    Added files

---
+++ app/app.js
@@ -0,0 +1,24 @@
+(function() {
+  
+  'use strict';
+  // Define the app module
+  angular.module('myApp', [
+    'ngMaterial',
+    'ngRoute',
+    'ngMessages'
+  ]).config(['$routeProvider', '$locationProvider',
+    function config($routeProvider, $locationProvider) {
+
+      // $locationProvider.hashPrefix('');
+
+      $routeProvider
+        .when('/', {
+          template: '',
+          authenticated: false
+        })
+        .otherwise('/');
+
+      $locationProvider.html5Mode(true);
+    }
+  ])
+})();
